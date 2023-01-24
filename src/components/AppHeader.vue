df03d935e7c3cb29cf2eb632e02b2ad8dbb286c2
VictorGiuliano <victorgiuliano98@gmail.com>
2023-01-24 17:39:50 +0100

    Make header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,14 +4,31 @@ export default {
 }
 </script>
 <template>
-    <header>
-        <div class="">
-
+    <header class="container">
+        <div class="pokedex-button">
+            <img src="../assets/scss/img/gold.png">
+            <img src="../assets/scss/img/blu.png">
+            <img src="../assets/scss/img/green.png">
         </div>
+        <img src="../assets/scss/img/pokemon-logo.png">
     </header>
 </template>
-<style lang="scss">
+<style lang="scss" scoped>
+@use '../assets/scss/partials/mixin' as *;
+@use '../assets/scss/style.scss' as *;
+
 header {
     height: 100px;
+    @include my-display-flex;
+}
+
+.pokedex-button {
+    @include my-display-flex;
+    align-items: center;
+
+    img {
+        width: auto;
+        max-height: 40%;
+    }
 }
 </style>
