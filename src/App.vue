1f5939d94b11f53d7bfc4d75925df97f0509bfc0
VictorGiuliano <victorgiuliano98@gmail.com>
2023-01-24 17:02:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name: 'Pokedex',
+  components: { AppHeader },
+}
+
+</script>
+<template>
+  <AppHeader />
+</template>
+<style lang="scss">
+@use './assets/scss/style.scss';
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+<template>
+    <header>
+        <div class="">
+
+        </div>
+    </header>
+</template>
+<style lang="scss">
+header {
+    height: 100px;
+}
+</style>
